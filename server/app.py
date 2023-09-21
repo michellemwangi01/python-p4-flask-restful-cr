2ca71f263731a11bafc13e5a561da4cb09449aee
michellemwangi01 <michellemwangim@gmail.com>
2023-09-22 01:16:43 +0300

    commito

--- server/app.py
+++ server/app.py
@@ -16,8 +16,43 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class Home(Resource):
-    pass
+    def get(self):
+        response_dict = {
+            "index": "Welcome to the Newsletter RESTFul API",
+        }
+        response = make_response(response_dict, 200)
+        return response
+
+
+api.add_resource(Home, '/')
+
+
+class Newsletters(Resource):
+    def get(self):
+        newsletter_dicts = [newsletter.to_dict() for newsletter in Newsletter.query.all()]
+        return make_response(newsletter_dicts, 200)
+
+    def post(self):
+        new_newsletter = Newsletter(
+            title=request.form['title'],
+            body=request.form['body']
+        )
+        db.session.add(new_newsletter)
+        db.session.commit()
+        return make_response(new_newsletter.to_dict(), 201)
+
+
+api.add_resource(Newsletters, '/newsletters')
+
+
+class NewsLetterByID(Resource):
+    def get(self, id):
+       return make_response(Newsletter.query.filter_by(id=id).first().to_dict(),200)
+
+
+api.add_resource(NewsLetterByID, '/newsletterbyid/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
